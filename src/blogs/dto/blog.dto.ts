6f81e143e7606d67c4aa5862ca84fe027f347e79
Abduljebar Sani <Abduljebar49@gmail.com>
2026-01-12 09:52:51 +0300

    perf(scaffold): optimize config

---
+++ src/blogs/dto/blog.dto.ts
@@ -0,0 +1,7 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty, IsOptional, IsString, IsUrl } from 'class-validator';
+import { ContentLanguage } from '../../shared/enums/content-language.enum';
+
+export class CreateBlogDto {
+    @ApiProperty()
+    @IsString()
