93bb2b3a6aaa5a749a8fd0a670de85b897cb97da
Abduljebar Sani <Abduljebar49@gmail.com>
2026-01-15 13:15:41 +0300

    feat(scaffold): implement date range picker

---
+++ src/engine/entities/game-progress.entity.ts
@@ -0,0 +1,10 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { User } from '../../users/entities/user.entity';
