345315f636eca47e68a8a102f50962b68d973cde
Abduljebar Sani <Abduljebar49@gmail.com>
2026-02-02 08:41:23 +0300

    perf(scoring): optimize chart

--- src/engine/entities/player-action.entity.ts
+++ src/engine/entities/player-action.entity.ts
@@ -5,3 +5,9 @@ import {
   CreateDateColumn,
   ManyToOne,
   JoinColumn,
+} from 'typeorm';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { User } from '../../users/entities/user.entity';
+import { Scenario } from './scenario.entity';
+import { Scene } from './scene.entity';
+import { GameProgress } from './game-progress.entity';
