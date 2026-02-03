6458c3bd63dc99917741f892b6958738e2ef6bc6
Abduljebar Sani <Abduljebar49@gmail.com>
2026-02-03 15:42:43 +0300

    fix(engine): harden session handling (Closes #32)

---
+++ src/engine/dto/create-scene.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, IsInt, IsString, IsBoolean, IsEnum, IsArray, ValidateNested, IsUUID, IsObject } from 'class-validator';
+import { Type } from 'class-transformer';
+import { SceneContentType } from '../../shared/enums/scene-content-type.enum';
+import { PlayerActionType } from '../../shared/enums/player-action-type.enum';
+import { OutcomeType } from '../../shared/enums/outcome-type.enum';
+
+export class CreateGameOutcomeDto {
+    @ApiProperty({ enum: OutcomeType })
+    @IsEnum(OutcomeType)
+    outcomeType: OutcomeType;
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsInt()
+    score?: number;
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsInt()
+    trustScoreDelta?: number;
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsString()
+    message?: string;
+
+    @ApiPropertyOptional()
+    @IsOptional()
