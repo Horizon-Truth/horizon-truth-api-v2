c8bce27ba8569960d2414c96d32feb03a5fe1492
Abduljebar Sani <Abduljebar49@gmail.com>
2026-02-05 16:10:20 +0300

    docs: refresh data model docs

---
+++ src/analytics/entities/analytics-snapshot.entity.ts
@@ -0,0 +1,10 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Organization } from '../../organizations/entities/organization.entity';
