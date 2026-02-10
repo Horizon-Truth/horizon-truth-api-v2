2cd12e09bb91e9528639cf2bd0c0028eeb7f7a09
Abduljebar Sani <Abduljebar49@gmail.com>
2026-02-10 13:07:29 +0300

    test(engine): add tests for test

--- src/engine/entities/scene.entity.ts
+++ src/engine/entities/scene.entity.ts
@@ -6,3 +6,12 @@ import {
   ManyToOne,
   JoinColumn,
   OneToMany,
+  OneToOne,
+} from 'typeorm';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Scenario } from './scenario.entity';
+import { SceneContentType } from '../../shared/enums/scene-content-type.enum';
+import { SceneContent } from './scene-content.entity';
+import { PlayerChoice } from './player-choice.entity';
+
+@Entity('scenes')
