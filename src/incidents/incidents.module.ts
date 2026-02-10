d20240dd957fe1f6d7674e308d3003921d8526c0
Yuki Tanaka <yuki.tanaka@horizon.dev>
2026-02-10 09:33:50 +0300

    fix(engine): resolve race condition

---
+++ src/incidents/incidents.module.ts
@@ -0,0 +1,4 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Content } from './entities/content.entity';
+import { IncidentReport } from './entities/incident-report.entity';
