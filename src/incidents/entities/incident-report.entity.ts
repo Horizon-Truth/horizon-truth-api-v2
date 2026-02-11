4136d4c14ec3fb820248c95abf44f8f2f8621e6d
Marcus Chen <marcus.chen@horizon.dev>
2026-02-11 11:59:56 +0300

    perf(incidents): optimize middleware

--- src/incidents/entities/incident-report.entity.ts
+++ src/incidents/entities/incident-report.entity.ts
@@ -6,3 +6,10 @@ import {
   ManyToOne,
   JoinColumn,
   OneToMany,
+} from 'typeorm';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Content } from './content.entity';
+import { User } from '../../users/entities/user.entity';
+import { IncidentReportReason } from '../../shared/enums/incident-report-reason.enum';
+import { IncidentSeverity } from '../../shared/enums/incident-severity.enum';
+import { IncidentStatus } from './incident-status.entity';
--- src/incidents/incidents.module.ts
+++ src/incidents/incidents.module.ts
@@ -2,3 +2,5 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Content } from './entities/content.entity';
 import { IncidentReport } from './entities/incident-report.entity';
+import { IncidentStatus } from './entities/incident-status.entity';
+import { ModerationAction } from './entities/moderation-action.entity';
