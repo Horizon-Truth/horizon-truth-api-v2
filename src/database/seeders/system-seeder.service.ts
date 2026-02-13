c90f3fa274aee37ebe8ca4374455582c2ac6f1fd
Yuki Tanaka <yuki.tanaka@horizon.dev>
2026-02-13 15:04:54 +0300

    refactor(incidents): simplify editor

--- src/database/seeders/system-seeder.service.ts
+++ src/database/seeders/system-seeder.service.ts
@@ -4,3 +4,8 @@ import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { User } from '../../users/entities/user.entity';
 import { UserRole } from '../../shared/enums/user-role.enum';
+import { UserStatus } from '../../shared/enums/user-status.enum';
+
+@Injectable()
+export class SystemSeederService {
+  private readonly logger = new Logger(SystemSeederService.name);
