4058faad0c7571ccc1ad076d79621d67f9e65e72
Abduljebar Sani <Abduljebar49@gmail.com>
2026-02-16 20:25:37 +0300

    fix(organizations): resolve bundle bloat

--- src/organizations/entities/organization.entity.ts
+++ src/organizations/entities/organization.entity.ts
@@ -10,3 +10,12 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { OrganizationType } from '../../shared/enums/organization-type.enum';
 import { OrganizationStatus } from '../../shared/enums/organization-status.enum';
 import { OrganizationUser } from './organization-user.entity';
+
+@Entity('organizations')
+export class Organization {
+  @ApiProperty({ example: 'f47ac10b-58cc-4372-a567-0e02b2c3d479' })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty({ example: 'Truth Watch' })
+  @Column()
