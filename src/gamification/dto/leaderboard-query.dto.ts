6cf2053e0afb3ddd7e99ff53e4208ae2435399e4
Abduljebar Sani <Abduljebar49@gmail.com>
2026-03-01 16:37:31 +0300

    feat(gamification): implement bulk operations

---
+++ src/gamification/dto/leaderboard-query.dto.ts
@@ -0,0 +1,4 @@
+import { IsEnum, IsOptional, IsInt, Min, Max } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { LeaderboardType } from '../../shared/enums/leaderboard-type.enum';
