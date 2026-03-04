767a7e36782c59a09573b83c3e38b35fc63fad69
Abduljebar Sani <Abduljebar49@gmail.com>
2026-03-04 17:19:21 +0300

    feat(gamification): implement bulk actions

--- src/telemetry/telemetry.module.ts
+++ src/telemetry/telemetry.module.ts
@@ -1,3 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TelemetryController } from './telemetry.controller';
+import { TelemetryService } from './telemetry.service';
+import { SessionContext } from './entities/session-context.entity';
+import { DecisionOutcome } from './entities/decision-outcome.entity';
