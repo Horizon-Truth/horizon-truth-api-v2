88b4c4938832aa6b231725c44195309cc25db69f
Abduljebar Sani <Abduljebar49@gmail.com>
2026-03-04 08:19:14 +0300

    feat(gamification): implement error states

--- src/gamification/dto/leaderboard-query.dto.ts
+++ src/gamification/dto/leaderboard-query.dto.ts
@@ -2,3 +2,7 @@ import { IsEnum, IsOptional, IsInt, Min, Max } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { LeaderboardType } from '../../shared/enums/leaderboard-type.enum';
+import { LeaderboardPeriod } from '../../shared/enums/leaderboard-period.enum';
+
+export class LeaderboardQueryDto {
+  @ApiProperty({
