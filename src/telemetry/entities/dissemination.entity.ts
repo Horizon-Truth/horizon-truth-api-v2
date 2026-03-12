92a3f7b32d55859f0e6d2282b7e6ab4e268e1ad1
Abduljebar Sani <Abduljebar49@gmail.com>
2026-03-12 11:05:55 +0300

    fix(telemetry): resolve memory pressure

--- src/telemetry/entities/dissemination.entity.ts
+++ src/telemetry/entities/dissemination.entity.ts
@@ -2,3 +2,6 @@ import { Entity, Column, PrimaryColumn, CreateDateColumn } from 'typeorm';
 
 export enum ShareChannelType {
     PUBLIC = 'public',
+    PRIVATE = 'private',
+    GROUP = 'group',
+}
