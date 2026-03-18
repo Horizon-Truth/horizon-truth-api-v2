f6a1bfff36c7b85a734ac9b0be52a225fb01e528
Ravi Patel <ravi.patel@horizon.dev>
2026-03-18 08:50:21 +0300

    feat(players): implement autocomplete

---
+++ src/players/players.service.spec.ts
@@ -0,0 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PlayersService } from './players.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { PlayerProfile } from './entities/player-profile.entity';
+import { Avatar } from './entities/avatar.entity';
+import { Region } from './entities/region.entity';
+import { PlayerAlgorithmProfile } from '../analytics/entities/player-algorithm-profile.entity';
+import { PlayerLearningProfile } from './entities/player-learning-profile.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+
+describe('PlayersService', () => {
+  let service: PlayersService;
+  let playerProfileRepository: any;
+  let avatarRepository: any;
+  let regionRepository: any;
+  let algorithmProfileRepository: any;
+
+  const mockPlayerProfileRepository = {
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    manager: {
+        createQueryBuilder: jest.fn(() => ({
+            select: jest.fn().mockReturnThis(),
+            addSelect: jest.fn().mockReturnThis(),
+            from: jest.fn().mockReturnThis(),
+            where: jest.fn().mockReturnThis(),
