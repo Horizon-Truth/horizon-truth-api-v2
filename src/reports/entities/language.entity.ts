151cf3a3baebe5aca1b3512bae26c36c5b5d3e93
Abduljebar Sani <Abduljebar49@gmail.com>
2026-03-23 15:52:51 +0300

    feat(reports): implement infinite scroll

---
+++ src/reports/entities/language.entity.ts
@@ -0,0 +1,3 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
--- src/reports/report-tags.service.ts
+++ src/reports/report-tags.service.ts
@@ -8,3 +8,14 @@ import { Repository } from 'typeorm';
 import { ReportTag } from './entities/report-tag.entity';
 import { CreateReportTagDto } from './dto/create-report-tag.dto';
 import { UpdateReportTagDto } from './dto/update-report-tag.dto';
+
+@Injectable()
+export class ReportTagsService {
+  constructor(
+    @InjectRepository(ReportTag)
+    private readonly reportTagRepository: Repository<ReportTag>,
+  ) {}
+
+  async findAll(query: any): Promise<any> {
+    const { isActive, page = 1, limit = 10, search } = query;
+    const skip = (page - 1) * limit;
