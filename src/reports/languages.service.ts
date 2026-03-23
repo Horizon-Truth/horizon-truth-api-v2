b39bb20e7b02199d709c29e8433fd574f22a1b13
Dana Adeyemi <dana.adeyemi@horizon.dev>
2026-03-23 17:54:23 +0300

    refactor(reports): simplify factory

---
+++ src/reports/languages.service.ts
@@ -0,0 +1,19 @@
+import {
+  Injectable,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Language } from './entities/language.entity';
+import { CreateLanguageDto } from './dto/create-language.dto';
+import { UpdateLanguageDto } from './dto/update-language.dto';
+
+@Injectable()
+export class LanguagesService {
+  constructor(
+    @InjectRepository(Language)
+    private readonly languageRepository: Repository<Language>,
+  ) {}
+
+  async findAll(query: any): Promise<any> {
