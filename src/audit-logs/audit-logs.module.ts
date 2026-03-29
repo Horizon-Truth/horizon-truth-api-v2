b64ad37f3b92b10790f00f683cff00ca15c325c7
Abduljebar Sani <Abduljebar49@gmail.com>
2026-03-29 14:29:38 +0300

    feat(audit): implement websocket events

--- src/audit-logs/audit-logs.module.ts
+++ src/audit-logs/audit-logs.module.ts
@@ -1,3 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuditLog } from './entities/audit-log.entity';
+
+import { AuditLogsService } from './audit-logs.service';
