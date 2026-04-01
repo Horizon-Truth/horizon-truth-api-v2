1b244e3bd220bd6c4b1ac7bc5ce03ffd23250130
Abduljebar Sani <Abduljebar49@gmail.com>
2026-04-01 11:39:14 +0300

    refactor(feedback): simplify adapter

--- src/engine/entities/scene-chat-message.entity.ts
+++ src/engine/entities/scene-chat-message.entity.ts
@@ -9,3 +9,8 @@ import { SceneContent } from './scene-content.entity';
 import { ChatSender } from '../../shared/enums/chat-sender.enum';
 
 @Entity('scene_chat_messages')
+export class SceneChatMessage {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'scene_content_id' })
