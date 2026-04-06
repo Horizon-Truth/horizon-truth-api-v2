1abd4d761d618184c327c89f42e7c08a4420abfa
Abduljebar Sani <Abduljebar49@gmail.com>
2026-04-06 09:22:51 +0300

    feat(contacts): implement login flow

--- src/contacts/dto/create-contact.dto.ts
+++ src/contacts/dto/create-contact.dto.ts
@@ -1,2 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateContactDto {
+    @ApiProperty({ example: 'John' })
+    @IsNotEmpty()
+    @IsString()
