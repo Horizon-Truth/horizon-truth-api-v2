f9353f6bbd752bfbdc22c293f859648e01cf5138
Abduljebar Sani <Abduljebar49@gmail.com>
2026-04-06 10:00:23 +0300

    style(contacts): clean up comments

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -7,3 +7,13 @@ import {
     Patch,
     Delete,
     Request,
+    UseGuards,
+} from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { ContactsService } from './contacts.service';
+import { CreateContactDto } from './dto/create-contact.dto';
+import { ReplyContactDto } from './dto/reply-contact.dto';
+import { Contact } from './entities/contact.entity';
+import { ContactReply } from './entities/contact-reply.entity';
+import { Public } from '../auth/decorators/public.decorator';
+import { Roles } from '../auth/decorators/roles.decorator';
---
+++ src/contacts/dto/reply-contact.dto.ts
@@ -0,0 +1,2 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
