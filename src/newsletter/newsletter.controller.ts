ac6a8231ae79e467c914edf5309c1e9bc26690bf
Marcus Chen <marcus.chen@horizon.dev>
2026-04-09 13:30:13 +0300

    feat(newsletter): implement registration flow

--- src/newsletter/newsletter.controller.ts
+++ src/newsletter/newsletter.controller.ts
@@ -3,3 +3,9 @@ import {
     Get,
     Post,
     Body,
+    Param,
+    Delete,
+    UseGuards,
+} from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { NewsletterService } from './newsletter.service';
