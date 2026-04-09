b53ebe9b7808e2f584cb49d010687f248ca4f666
Dana Adeyemi <dana.adeyemi@horizon.dev>
2026-04-09 08:46:34 +0300

    perf(newsletter): optimize dropdown

---
+++ src/newsletter/newsletter.service.ts
@@ -0,0 +1,7 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Newsletter } from './entities/newsletter.entity';
+import { CreateNewsletterDto } from './dto/create-newsletter.dto';
+
+@Injectable()
