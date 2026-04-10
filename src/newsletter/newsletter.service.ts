c07b75115ac78b6b1e83828679699cb57e91eb40
Yuki Tanaka <yuki.tanaka@horizon.dev>
2026-04-10 09:59:19 +0300

    feat(newsletter): implement responsive layout

--- src/newsletter/newsletter.service.ts
+++ src/newsletter/newsletter.service.ts
@@ -5,3 +5,7 @@ import { Newsletter } from './entities/newsletter.entity';
 import { CreateNewsletterDto } from './dto/create-newsletter.dto';
 
 @Injectable()
+export class NewsletterService {
+    constructor(
+        @InjectRepository(Newsletter)
+        private newsletterRepository: Repository<Newsletter>,
