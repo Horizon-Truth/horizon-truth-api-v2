dd8e0882a6a018b6cc1925ef7e8b98c659134aba
Abduljebar Sani <Abduljebar49@gmail.com>
2026-04-21 13:59:29 +0300

    test(import-export): add tests for editor

--- src/engine/entities/scene-feed-item.entity.ts
+++ src/engine/entities/scene-feed-item.entity.ts
@@ -10,3 +10,7 @@ import { SceneContent } from './scene-content.entity';
 @Entity('scene_feed_items')
 export class SceneFeedItem {
   @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'scene_content_id' })
+  sceneContentId: string;
