9a805d6c9acde740f70167dd65c0ae5876208bc6
Ravi Patel <ravi.patel@horizon.dev>
2026-04-23 10:32:27 +0300

    fix(admin-tools): resolve unhandled rejection

---
+++ src/seed.ts
@@ -0,0 +1,4 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { GameSeederService } from './database/seeders/game-seeder.service';
+import { SystemSeederService } from './database/seeders/system-seeder.service';
