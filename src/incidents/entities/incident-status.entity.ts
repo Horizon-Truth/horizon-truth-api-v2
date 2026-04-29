007543821ff17e10512682b39cbf4fc1479464c4
Abduljebar Sani <Abduljebar49@gmail.com>
2026-04-29 10:17:49 +0300

    fix(postgres): resolve incorrect sort order

--- src/incidents/entities/incident-status.entity.ts
+++ src/incidents/entities/incident-status.entity.ts
@@ -14,3 +14,9 @@ import { IncidentStatusType } from '../../shared/enums/incident-status-type.enum
 @Entity('incident_statuses')
 export class IncidentStatus {
   @ApiProperty({ example: 'f47ac10b-58cc-4372-a567-0e02b2c3d479' })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty({ example: 'f47ac10b-58cc-4372-a567-0e02b2c3d479' })
+  @Column({ name: 'incident_report_id' })
+  incidentReportId: string;
