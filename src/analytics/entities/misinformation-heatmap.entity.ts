eeaa8a1580ad2f6221fd1c47c4d302458b696df4
Abduljebar Sani <Abduljebar49@gmail.com>
2026-05-14 16:58:13 +0300

    ci: improve build steps

--- src/analytics/entities/misinformation-heatmap.entity.ts
+++ src/analytics/entities/misinformation-heatmap.entity.ts
@@ -9,3 +9,8 @@ import {
 import { Organization } from '../../organizations/entities/organization.entity';
 import { HeatmapRegionType } from '../../shared/enums/heatmap-region-type.enum';
 import { HeatmapRiskLevel } from '../../shared/enums/heatmap-risk-level.enum';
+
+@Entity('misinformation_heatmaps')
+export class MisinformationHeatmap {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
