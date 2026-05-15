9f8f527f6325168ccd2c3232dbbcfa979b617507
Abduljebar Sani <Abduljebar49@gmail.com>
2026-05-15 09:55:54 +0300

    perf(misc): optimize analytics

--- src/engine/dto/start-game.dto.ts
+++ src/engine/dto/start-game.dto.ts
@@ -3,3 +3,9 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class StartGameDto {
   @ApiProperty({
+    description: 'ID of the scenario to start',
+    example: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+  })
+  @IsUUID()
+  scenarioId: string;
+}
