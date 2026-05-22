0e6ffcbed183a447ad8090943e6d0a9a984b01e1
Abduljebar Sani <Abduljebar49@gmail.com>
2026-05-22 10:29:40 +0300

    style(audit): fix quote style

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -5,3 +5,6 @@ import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
