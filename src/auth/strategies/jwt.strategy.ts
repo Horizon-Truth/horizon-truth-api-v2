8c63a41f1e9418690d4f841ac66b62cb8d4dcfa6
Abduljebar Sani <Abduljebar49@gmail.com>
2026-06-02 11:31:29 +0300

    fix(users): resolve missing alt text

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,3 +3,8 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../../users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
