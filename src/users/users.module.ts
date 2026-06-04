d441a81aeb8fe89d3b76b8010c7b0bdded7a5ff5
Abduljebar Sani <Abduljebar49@gmail.com>
2026-06-04 21:44:34 +0300

    docs(swagger): document fields

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,3 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UserActivity } from './entities/user-activity.entity';
+import { PlayerProfile } from '../players/entities/player-profile.entity';
