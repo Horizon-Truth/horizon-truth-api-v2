e961d46d3b70d86ce2b225d5b07a95a9a6e1c0cb
Marcus Chen <marcus.chen@horizon.dev>
2026-06-15 14:41:58 +0300

    fix(import-export): resolve incorrect sort order

--- src/feedback/feedback.module.ts
+++ src/feedback/feedback.module.ts
@@ -2,3 +2,4 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Feedback } from './entities/feedback.entity';
 import { FeedbackService } from './feedback.service';
+import { FeedbackController } from './feedback.controller';
