c8d249fb686893a273ba1499078d6eef58188ddc
Abduljebar Sani <Abduljebar49@gmail.com>
2026-06-16 17:05:50 +0300

    fix(swagger): resolve date parsing bug

--- src/feedback/feedback.module.ts
+++ src/feedback/feedback.module.ts
@@ -3,3 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Feedback } from './entities/feedback.entity';
 import { FeedbackService } from './feedback.service';
 import { FeedbackController } from './feedback.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Feedback])],
+  controllers: [FeedbackController],
+  providers: [FeedbackService],
+  exports: [FeedbackService],
+})
+export class FeedbackModule {}
