ab619c1e15b98fb4509c04e477441bc0664dbce3
Yuki Tanaka <yuki.tanaka@horizon.dev>
2026-07-10 08:01:33 +0300

    docs(auth): document resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,3 +16,76 @@ import { EngineModule } from './engine/engine.module';
 import { IncidentsModule } from './incidents/incidents.module';
 import { GamificationModule } from './gamification/gamification.module';
 import { AnalyticsModule } from './analytics/analytics.module';
+import { SeederModule } from './database/seeders/seeder.module';
+import { ReportsModule } from './reports/reports.module';
+import { LanguagesModule } from './reports/languages.module';
+import { FeedbackModule } from './feedback/feedback.module';
+import { BlogsModule } from './blogs/blogs.module';
+import { ResourcesModule } from './resources/resources.module';
+import { ContactsModule } from './contacts/contacts.module';
+import { NewsletterModule } from './newsletter/newsletter.module';
+import { TelemetryModule } from './telemetry/telemetry.module';
+import { MailModule } from './mail/mail.module';
+import { GlobalExceptionFilter } from './shared/filters/global-exception.filter';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MailModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // 1 minute window
+        limit: 100, // 100 requests per minute per IP (much more reasonable)
+      },
+    ]),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    AuthModule,
+    OrganizationsModule,
+    AuditLogsModule,
+    PlayersModule,
+    EngineModule,
+    IncidentsModule,
+    GamificationModule,
+    AnalyticsModule,
+    SeederModule,
+    ReportsModule,
+    LanguagesModule,
+    FeedbackModule,
+    BlogsModule,
+    ResourcesModule,
+    ContactsModule,
+    NewsletterModule,
+    TelemetryModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AuditLogInterceptor,
+    },
+  ],
+})
+export class AppModule { }
