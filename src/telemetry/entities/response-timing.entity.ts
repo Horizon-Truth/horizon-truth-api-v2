9948d89bcbc7caa108e66438137401295e403921
Abduljebar Sani <Abduljebar49@gmail.com>
2026-07-21 08:30:36 +0300

    refactor(setup): simplify store

--- src/telemetry/entities/response-timing.entity.ts
+++ src/telemetry/entities/response-timing.entity.ts
@@ -3,3 +3,6 @@ import { Entity, Column, PrimaryColumn, CreateDateColumn } from 'typeorm';
 @Entity('telemetry_response_timing')
 export class ResponseTiming {
     @PrimaryColumn()
+    session_id: string;
+
+    @Column({ type: 'timestamp' })
