acdd2892b91bdf8e671d6a21fd441fcb358ca711
Abduljebar Sani <Abduljebar49@gmail.com>
2026-07-27 20:02:58 +0300

    fix(ui): deadlock

--- src/players/dto/initialize-profile.dto.ts
+++ src/players/dto/initialize-profile.dto.ts
@@ -2,3 +2,5 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsUUID, IsOptional } from 'class-validator';
 
 export class InitializeProfileDto {
+  @ApiProperty({ example: 'FactChecker99' })
+  @IsString()
